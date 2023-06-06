9b0fac1a0352e18cc727ed7047297ab5cf0256bd
niera26 <niera26@outlook.com>
2023-06-06 11:02:59 +0200

    configure deploy script

---
+++ script/Deploy.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Script.sol";
+import {BehStakingPool} from "../src/BehStakingPool.sol";
+
+contract Deploy is Script {
+    function setUp() public {}
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        address stakingTokenAddress = vm.envAddress("STAKING_TOKEN_ADDRESS");
+        address rewardsTokenAddress = vm.envAddress("REWARDS_TOKEN_ADDRESS");
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        new BehStakingPool(stakingTokenAddress, rewardsTokenAddress);
+
+        vm.stopBroadcast();
+    }
+}
