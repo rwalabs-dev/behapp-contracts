469d231e7667c7a120ffce2587689db93923df2f
niera26 <niera26@outlook.com>
2023-06-15 14:50:31 +0200

    update to staking contract with better default admin role

--- src/BehStakingPool.sol
+++ src/BehStakingPool.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.17;
 import {ERC20StakingPool} from "staking-contracts/ERC20StakingPool.sol";
 
 contract BehStakingPool is ERC20StakingPool {
-    constructor(address stakingToken, address rewardsToken) ERC20StakingPool(stakingToken, rewardsToken) {}
+    constructor(address stakingToken, address rewardsToken) ERC20StakingPool(stakingToken, rewardsToken, 1) {}
 }
